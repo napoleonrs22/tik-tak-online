c19d5f74d9b5afd1f2ff3a250bfbd4c0f5928ec4
Erasyl <erasylam@yandex.kz>
2025-05-25 20:44:31 +0500

    feat prettier

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,29 @@
-const { PrismaClient } = require('../src/generated/prisma/client');
+const { PrismaClient } = require("../src/generated/prisma/client");
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 async function main() {
-    await prisma.game.create({
-        data:{
-            name: 'game-1'
-        }
-    })
-    await prisma.game.create({
-        data:{
-            name: 'game-2'
-        }
-    })
-    await prisma.game.create({
-        data:{
-            name: 'game-3'
-        }
-    })
-
+  await prisma.game.create({
+    data: {
+      name: "game-1",
+    },
+  });
+  await prisma.game.create({
+    data: {
+      name: "game-2",
+    },
+  });
+  await prisma.game.create({
+    data: {
+      name: "game-3",
+    },
+  });
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,21 @@ import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
 import { Card, CardTitle } from "@/shared/ui/card";
 
-
-export default async  function Home() {
-  
+export default async function Home() {
   const games = await prisma.game.findMany();
 
-  console.log(games)
+  console.log(games);
   return (
     <div className="">
-      <Button size="lg" variant="destructive">Hello</Button>
-      {games.map(game =>{
-        return <Card key={game.id}>
-          <CardTitle>{game.name}</CardTitle>
-        </Card>
+      <Button size="lg" variant="destructive">
+        Hello
+      </Button>
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
       })}
     </div>
   );
